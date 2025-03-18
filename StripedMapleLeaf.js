8e14440c803f4fa587f3051c7b9657e06dd326eb
charunyak2005 <charunya.krishnamurthy@smu.ca>
2025-03-18 12:44:04 -0300

    Add files via upload

---
+++ StripedMapleLeaf.js
@@ -0,0 +1,12 @@
+ 
+function Soundplay() {
+    document.getElementById("sound").play();
+}
+
+function setup(){
+    document.getElementById("page").addEventListener("click" , Soundplay , false);
+}
+
+function backLandPage() {
+    window.location.href = "index.html"; 
+}
